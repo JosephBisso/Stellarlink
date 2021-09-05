cdd349eca64285b18a64c26e026a00e00001cfac
JosephBisso <j.bisso-bi-ela@tu-braunschweig.de>
2021-09-06 00:42:33 +0200

    changed qt modules and update ci

--- Stellarlink_en_DE.ts
+++ Stellarlink_en_DE.ts
@@ -1,3 +1,12 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en_DE"></TS>
+<TS version="2.1" language="en_DE">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="main.qml" line="8"/>
+        <source>Hello World</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
--- main.cpp
+++ main.cpp
@@ -6,8 +6,6 @@
 
 int main(int argc, char *argv[])
 {
-    qputenv("QT_IM_MODULE", QByteArray("qtvirtualkeyboard"));
-
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
