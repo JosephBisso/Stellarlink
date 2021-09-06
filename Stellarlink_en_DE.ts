52fff2025150c24eed720ce9c16629380e0502bf
JosephBisso <j.bisso-bi-ela@tu-braunschweig.de>
2021-09-06 16:07:26 +0200

    initial commit

--- Stellarlink_en_DE.ts
+++ Stellarlink_en_DE.ts
@@ -1,12 +1,3 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en_DE">
-<context>
-    <name>main</name>
-    <message>
-        <location filename="main.qml" line="8"/>
-        <source>Hello World</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
-</TS>
+<TS version="2.1" language="en_DE"></TS>
